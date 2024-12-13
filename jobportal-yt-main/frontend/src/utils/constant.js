babe99651c48802416db98b9f482ebbfd0f23fed
tushar patel <tushar2212099@akgec.ac.in>
2024-12-13 23:28:34 +0530

    last

--- jobportal-yt-main/frontend/src/utils/constant.js
+++ jobportal-yt-main/frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="https://vercel.com/tushars-projects-749fed38/job-hunt/5Z7QspqU8kGwMRgSya5oiDc78koh/api/v1/user";
-export const JOB_API_END_POINT="https://vercel.com/tushars-projects-749fed38/job-hunt/5Z7QspqU8kGwMRgSya5oiDc78koh/api/v1/job";
-export const APPLICATION_API_END_POINT="https://vercel.com/tushars-projects-749fed38/job-hunt/5Z7QspqU8kGwMRgSya5oiDc78koh/api/v1/application";
-export const COMPANY_API_END_POINT="https://vercel.com/tushars-projects-749fed38/job-hunt/5Z7QspqU8kGwMRgSya5oiDc78koh/api/v1/company";
+export const USER_API_END_POINT="https://job-hunt-umber.vercel.app//api/v1/user";
+export const JOB_API_END_POINT="https://job-hunt-umber.vercel.app//api/v1/job";
+export const APPLICATION_API_END_POINT="https://job-hunt-umber.vercel.app//api/v1/application";
+export const COMPANY_API_END_POINT="https://job-hunt-umber.vercel.app//api/v1/company";
